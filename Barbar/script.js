b3ea8658598f44d038102138db225e280a9469d7
pritamkumarpatna59@gmail.com <pritamkumarpatna@gmail.com>
2026-08-23 22:25:54 +0530

    Barber Shop Website Design
    Navbar and Feature Section complete.

---
+++ Barbar/script.js
@@ -0,0 +1,17 @@
+var menuBtn = document.getElementById("menubtn");
+var sidenav = document.getElementById("sidenav");
+var menu= document.getElementById("menu");
+
+sidenav.style.right ="-250px";
+
+menuBtn.onclick = function(){
+    if(sidenav.style.right =="-250px"){
+        sidenav.style.right ="0";
+        menu.src ="images/close.png";
+    }
+    else{
+        sidenav.style.right = "-250px";
+        menu.src ="images/menu.png";
+    }
+
+}
